018c7f4b2c129c35d127b691c0406a9ebb1df010
Jair Gonzalez <jairandres24@hotmail.es>
2022-11-24 09:21:09 -0500

    big bang

---
+++ paginas estaticas/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('#nav-icon').click(function(){
+        $(this).toggleClass('open');
+        $('.card').toggleClass('active');
+
+    });
+
+
+    $('.icon.close').click(function () {
+        $('#nav-icon').toggleClass('open');
+        $('.card').toggleClass('active');
+    });
+});
+
+
+
