53b1d7a35ff6d0ffc32bac7b218b307c3c84b409
Jair Gonzalez <jairandres24@hotmail.es>
2024-02-19 20:02:32 -0500

    fixes creditos

--- src/components/ModalInfo.jsx
+++ src/components/ModalInfo.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 export function ModalInfo() {
+  const datos_creditos = Object.values(require('../data/creditos.json'))
+
   return (
     <div className={'card card-default overflow-hidden bg-slate-100'}>
       {/*header of the card*/}
       <div className="card-body mx-3">
         <div className="mx-5 mt-4 flex drop-shadow-md">
           <div className=" card-title w-11/12 pl-10 text-center">
-            <h1 className="text-2xl lg:text-4xl">Sobre la Exposición</h1>
+            <h1 className="text-2xl lg:text-4xl">Grupo Curatorial</h1>
           </div>
           <div className="w-1/12 text-end">
             <div className="icon close">
@@ -20,57 +21,38 @@ export function ModalInfo() {
         </div>
         <br />
         <div id="customScroll" className="top-/[70px] sticky" style={{ maxHeight: 'calc(90vh - 120px)' }}>
-          <div className="card-text mx-10 text-justify text-sm lg:text-lg">
-            <p className="card-text text-justify">
-              El proyecto se titula Re-leyendo artefactos: Puesta en valor de piezas arqueológicas a través de una
-              museografía participativa en la Universidad de Santander, es resultado de la primera convocatoria que la
-              Universidad realiza en la modalidad de Creación Investigación UDES.
-              <br />
-              <br />
-              Es un trabajo interdisciplinario que conjugó perspectivas antropológicas, arqueológicas, de diseño gráfico
-              e historia del cual se derivan varios productos y hallazgos que pueden apreciarse en este portal web, bajo
-              las categorías de: Estética, Tiempo, Arte-Facto y Galería.
-              <br />
-              <br />
-              Esta experiencia investigativa contó con la participación de un equipo de docentes investigadores al igual
-              que estudiantes de la Universidad de Santander:
-            </p>
+          <div className="card-text mx-4 text-justify text-sm lg:mx-10 lg:text-lg">
             {/*2 columnas de texto*/}
-            <div className="m-4 grid grid-cols-2 text-sm lg:text-lg">
-              <div className="card-text  mr-5 text-justify">
-                <ul className="list-outside list-disc space-y-2">
-                  <li>
-                    {' '}
-                    Grethel Alexandra M. Flórez Sierra, docente investigadora del Programa de Tecnología en Comunicación
-                    Gráfica
-                  </li>
-                  <li>Margarita María Durán Urrea, docente investigadora del Programa de Antropología</li>
-                  <li>Mónica Johanna Giedelmann Reyes, docente investigadora del Programa de Antropología</li>
-                  <li>Johana Rojas Sierra,docente investigadora del Programa Tecnología en Comunicación Gráfica</li>
-                  <li>Jair Andres González Ruíz, estudiante tesista del Programa de Ingeniería de Software</li>
-                </ul>
-              </div>
-              <div className="card-text  ml-5 text-justify">
-                <ul className="list-outside list-disc space-y-2">
-                  <li>David Verdooren Flórez, estudiante del Programa de Antropología</li>
-                  <li>Christian M. Orozco Pérez, estudiante del Programa de Antropología</li>
-                  <li>Valentina Guevara Rincón , estudiante del Programa de Tecnología en Comunicación Gráfica.</li>
-                  <li>
-                    Francisco Julián Romero Villamizar, estudiante del Programa de Tecnología en Diseño Gráfico
-                    Publicitario.
-                  </li>
-                  <li>Edgar Pico Ruíz, maestro y encargado de la reserva arqueológica UDES.</li>
-                </ul>
-              </div>
+            <div className="grid grid-cols-1 grid-rows-1 gap-8 text-xs lg:grid-cols-1 lg:text-sm">
+              {/*recorro todos los items en creditos.json*/}
+
+              {datos_creditos.map((credito, index) => (
+                <div className="mx-8 flex justify-center gap-4">
+                  <img
+                    className="aspect-square  w-24  object-contain lg:h-auto lg:w-1/4"
+                    src={'/assets/creditos/' + credito.img}
+                  />
+                  <div className="flex max-w-sm flex-col self-center">
+                    <b className="text-base lg:text-lg">{credito.Nombre}</b>
+                    <i>{credito.Titulo}</i>
+
+                    <p className="my-2 text-xs">{credito.Descripcion} </p>
+                    <p>{credito.Profesion}</p>
+                    <p className={'text-xs'}>{credito.correo}</p>
+                  </div>
+                </div>
+              ))}
             </div>
 
-            <p className="card-text text-justify text-sm lg:text-lg">
-              Un especial agradecimiento a los estudiantes del Programa de Antropología y Tecnología en Comunicación
-              Gráfica al igual que a María Fernanda Reyes Rodríguez por sus contribuciones que ayudaron a enriquecer
-              este proyecto.
+            <hr className="border-1 border-slate-300 py-6" />
+            <p className="text-xs lg:text-sm">
+              Un especial agradecimiento a los estudiantes de los programas de Antropología y Tecnología en Comunicación
+              Gráfica que contribuyeron a este proyecto. Especialmente a Christian Mauricio Orozco Pérez y David
+              Verdooren Flórez (Antropología) y, Valentina Guevara Rincón y Francisco Julián Romero Villamizar
+              (Comunicación Gráfica).
             </p>
 
-            <hr className="border-1 mt-6 border-slate-300" />
+            <hr className="border-1 border-slate-300 pt-6" />
             {/*footer of the card*/}
             <div className="card-footer p-3 text-center">
               {/*icon of a world*/}
